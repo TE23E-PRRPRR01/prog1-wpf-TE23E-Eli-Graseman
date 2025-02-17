cce2a8a8b47e9d1dbf66fb8bc48edb9614849dea
Eli Graseman <eli.graseman@gmail.com>
2025-02-17 13:21:16 +0100

    tränat på wpf och gjort melloaspp

---
+++ SlumptalApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumptalApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSlumpa(object sender, RoutedEventArgs e)
+    {
+        //Läsa av maxvärde
+        //int max = int.Parse(tbxMaxvärde.Text) + 1;
+
+        if(!int.TryParse(txbMax.Text, out int max))
+        {
+            max= 100;
+        }
+        
+        // Slumpa ett tal 1-100
+        int slumptal = Random.Shared.Next(1, max);
+
+        // Skriv ut slumptalet i textboxen
+    tbxResultat.Text = $"{slumptal}";
+    }
+
+}
---
+++ melloapp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace melloapp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+        int antalRöd = 0;
+        int antalBlå = 0;
+        int antalGrön = 0;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickRösta(object sender, RoutedEvent e)
+    {
+        if (sender == röd)
+        {
+            antalRöd++;
+        }
+        else if (sender == blå)
+        {
+            antalBlå++;
+        }
+        else if (sender == grön)
+        {
+            antalGrön++;
+        }
+
+        // För varje klick +1
+        tbxResultat.Text = $"Röd: {antalRöd}";
+    }
+}
